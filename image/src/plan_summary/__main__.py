62449d870fd235f1a5777d70f6622df0e297c69b
Alina Freydina <alina.f@reproto.com>
2023-10-31 19:42:25 +0300

    feat: updated plan summary

--- image/src/plan_summary/__main__.py
+++ image/src/plan_summary/__main__.py
@@ -22,10 +22,26 @@ def main() -> None:
     with open(sys.argv[1]) as f:
         plan = f.read()
 
-    if match := re.search(r'^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', plan, re.MULTILINE):
-        output('to_add', match[1])
-        output('to_change', match[2])
-        output('to_destroy', match[3])
+    # if match := re.search(r'^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', plan, re.MULTILINE):
+    #     output('to_add', match[1])
+    #     output('to_change', match[2])
+    #     output('to_destroy', match[3])
+
+
+
+    # Regular expression pattern
+    pattern = r'^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy'
+
+    # Find all matches in the plan
+    matches = re.finditer(pattern, plan, re.MULTILINE)
+
+    # Iterate through the matches and process each one
+    for match in matches:
+        output('to_add', match.group(1))
+        output('to_change', match.group(2))
+        output('to_destroy', match.group(3))
+
+
 
 if __name__ == '__main__':
     sys.exit(main())
